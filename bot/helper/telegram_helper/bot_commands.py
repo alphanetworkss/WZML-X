8b07f2e0146eb2cb9708bfc0bc6962c864d70e3e
alphanetworkss <160410546+alphanetworkss@users.noreply.github.com>
2024-07-27 23:03:57 +0530

    Update bot_commands.py
    Signed-off-by: alphanetworkss <160410546+alphanetworkss@users.noreply.github.com>

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -4,30 +4,30 @@ from bot import CMD_SUFFIX, config_dict
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = [f'mirror{CMD_SUFFIX}', f'm{CMD_SUFFIX}']
+        self.MirrorCommand = [f'mirror1{CMD_SUFFIX}', f'm1{CMD_SUFFIX}']
         self.QbMirrorCommand = [f'qbmirror{CMD_SUFFIX}', f'qm{CMD_SUFFIX}']
-        self.YtdlCommand = [f'ytdl{CMD_SUFFIX}', f'y{CMD_SUFFIX}']
-        self.LeechCommand = [f'leech{CMD_SUFFIX}', f'l{CMD_SUFFIX}']
+        self.YtdlCommand = [f'ytdl1{CMD_SUFFIX}', f'y1{CMD_SUFFIX}']
+        self.LeechCommand = [f'leech1{CMD_SUFFIX}', f'l1{CMD_SUFFIX}']
         self.QbLeechCommand = [f'qbleech{CMD_SUFFIX}', f'ql{CMD_SUFFIX}']
-        self.YtdlLeechCommand = [f'ytdlleech{CMD_SUFFIX}', f'yl{CMD_SUFFIX}']
+        self.YtdlLeechCommand = [f'ytdlleech1{CMD_SUFFIX}', f'yl1{CMD_SUFFIX}']
         if config_dict['SHOW_EXTRA_CMDS']:
             self.MirrorCommand.extend([f'unzipmirror{CMD_SUFFIX}', f'uzm{CMD_SUFFIX}', f'zipmirror{CMD_SUFFIX}', f'zm{CMD_SUFFIX}'])
             self.QbMirrorCommand.extend([f'qbunzipmirror{CMD_SUFFIX}', f'quzm{CMD_SUFFIX}', f'qbzipmirror{CMD_SUFFIX}', f'qzm{CMD_SUFFIX}'])
             self.YtdlCommand.extend([f'ytdlzip{CMD_SUFFIX}', f'yz{CMD_SUFFIX}'])
-            self.LeechCommand.extend([f'unzipleech{CMD_SUFFIX}', f'uzl{CMD_SUFFIX}', f'zipleech{CMD_SUFFIX}', f'zl{CMD_SUFFIX}'])
-            self.QbLeechCommand.extend([f'qbunzipleech{CMD_SUFFIX}', f'quzl{CMD_SUFFIX}', f'qbzipleech{CMD_SUFFIX}', f'qzl{CMD_SUFFIX}'])
-            self.YtdlLeechCommand.extend([f'ytdlzipleech{CMD_SUFFIX}', f'yzl{CMD_SUFFIX}'])
-        self.CloneCommand = [f'clone{CMD_SUFFIX}', f'c{CMD_SUFFIX}']
-        self.CountCommand = f'count{CMD_SUFFIX}'
-        self.DeleteCommand = f'del{CMD_SUFFIX}'
-        self.CancelMirror = f'cancel{CMD_SUFFIX}'
-        self.CancelAllCommand = [f'cancelall{CMD_SUFFIX}', 'cancellallbot']
+            self.LeechCommand.extend([f'unzipleech{CMD_SUFFIX}', f'uzl1{CMD_SUFFIX}', f'zipleech1{CMD_SUFFIX}', f'zl1{CMD_SUFFIX}'])
+            self.QbLeechCommand.extend([f'qbunzipleech1{CMD_SUFFIX}', f'quz1l{CMD_SUFFIX}', f'qbzipleech1{CMD_SUFFIX}', f'qzl{CMD_SUFFIX}'])
+            self.YtdlLeechCommand.extend([f'ytdlzipleech1{CMD_SUFFIX}', f'yzl1{CMD_SUFFIX}'])
+        self.CloneCommand = [f'clone1{CMD_SUFFIX}', f'c1{CMD_SUFFIX}']
+        self.CountCommand = f'count1{CMD_SUFFIX}'
+        self.DeleteCommand = f'del1{CMD_SUFFIX}'
+        self.CancelMirror = f'cancel1{CMD_SUFFIX}'
+        self.CancelAllCommand = [f'cancelall1{CMD_SUFFIX}', 'cancellallbot']
         self.ListCommand = f'list{CMD_SUFFIX}'
         self.SearchCommand = f'search{CMD_SUFFIX}'
-        self.StatusCommand = [f'status{CMD_SUFFIX}', f's{CMD_SUFFIX}', 'statusall']
-        self.UsersCommand = f'users{CMD_SUFFIX}'
-        self.AuthorizeCommand = [f'authorize{CMD_SUFFIX}', f'a{CMD_SUFFIX}']
-        self.UnAuthorizeCommand = [f'unauthorize{CMD_SUFFIX}', f'ua{CMD_SUFFIX}']
+        self.StatusCommand = [f'status1{CMD_SUFFIX}', f's1{CMD_SUFFIX}', 'statusall']
+        self.UsersCommand = f'users1{CMD_SUFFIX}'
+        self.AuthorizeCommand = [f'authorize1{CMD_SUFFIX}', f'a1{CMD_SUFFIX}']
+        self.UnAuthorizeCommand = [f'unauthorize1{CMD_SUFFIX}', f'ua1{CMD_SUFFIX}']
         self.AddBlackListCommand = [f'blacklist{CMD_SUFFIX}', f'bl{CMD_SUFFIX}']
         self.RmBlackListCommand = [f'rmblacklist{CMD_SUFFIX}', f'rbl{CMD_SUFFIX}']
         self.AddSudoCommand = f'addsudo{CMD_SUFFIX}'
@@ -56,6 +56,6 @@ class _BotCommands:
         self.MediaInfoCommand = [f'mediainfo{CMD_SUFFIX}', f'mi{CMD_SUFFIX}']
         self.MyDramaListCommand = f'mdl{CMD_SUFFIX}'
         self.GDCleanCommand = [f'gdclean{CMD_SUFFIX}', f'gc{CMD_SUFFIX}']
-        self.BroadcastCommand = [f'broadcast{CMD_SUFFIX}', f'bc{CMD_SUFFIX}']
+        self.BroadcastCommand = [f'broadcast1{CMD_SUFFIX}', f'bc1{CMD_SUFFIX}']
 
 BotCommands = _BotCommands()
